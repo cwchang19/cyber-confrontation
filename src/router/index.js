9bfb92c63115f66173edf6c7019e66e38d67b536
cwchang19 <1278306293@qq.com>
2022-11-04 16:21:04 +0800

    init

--- src/router/index.js
+++ src/router/index.js
@@ -42,18 +42,30 @@ export const constantRoutes = [
     component: () => import('@/views/404'),
     hidden: true
   },
-
+  
   {
     path: '/',
     component: Layout,
-    redirect: '/dashboard',
+    redirect: '/mainpage',
     children: [{
-      path: 'dashboard',
-      name: 'Dashboard',
-      component: () => import('@/views/dashboard/index'),
-      meta: { title: 'Dashboard', icon: 'dashboard' }
+      path: 'mainpage',
+      name: 'Mainpage',
+      component: () => import('@/views/mainpage/index'),
+      meta: { title: '首页', icon: 'dashboard' }
     }]
   },
+
+  // {
+  //   path: '/',
+  //   component: Layout,
+  //   redirect: '/dashboard',
+  //   children: [{
+  //     path: 'dashboard',
+  //     name: 'Dashboard',
+  //     component: () => import('@/views/dashboard/index'),
+  //     meta: { title: 'Dashboard', icon: 'dashboard' }
+  //   }]
+  // },
 
   {
     path: '/example',
